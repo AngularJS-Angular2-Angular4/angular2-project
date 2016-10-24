c3a49f0bda5477c14393b06c2d0c64b09b004176
tsukhu <tsukhu@hcl.com>
2016-10-24 17:40:03 +0530

    commented out billingComponent from the app routes

--- pctl.mod.ui/src/main/frontend/app/app.routes.ts
+++ pctl.mod.ui/src/main/frontend/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
 import { LocationsComponent } from './locations/locations.component';
 import { ContactUsComponent } from './common/contact-us/contact-us.component';
-import { BillingComponent } from './billing/billing.component';
+// import { BillingComponent } from './billing/billing.component';
 
 import { DataResolver } from './app.resolver';
 
@@ -15,7 +15,7 @@ export const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact-us', component: ContactUsComponent },
-  { path: 'billing', component: BillingComponent },
+ // { path: 'billing', component: BillingComponent },
   {
     path: 'locations',
     component: LocationsComponent
