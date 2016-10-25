b68e073dd97bb9fd321c5203e8ba5835f4039818
Begin Samuel <begin.samuel@hcl.com>
2016-10-25 17:12:15 +0530

    Added core module source files

---
+++ pctl.mod.core/src/main/java/com/centurylink/pctl/mod/core/security/config/AuthoritiesConstants.java
@@ -0,0 +1,19 @@
+package com.centurylink.pctl.mod.core.security.config;
+
+/**
+ * Constants for Spring Security authorities.
+ */
+public final class AuthoritiesConstants {
+
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    public static final String USER = "ROLE_USER";
+
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    public static final String JWT_TOKEN_HEADER_PARAM = "X-Authorization";
+
+
+    private AuthoritiesConstants() {
+    }
+}
---
+++ pctl.mod.core/src/main/java/com/centurylink/pctl/mod/core/security/config/SpringSecurityAuditorAware.java
@@ -0,0 +1,21 @@
+package com.centurylink.pctl.mod.core.security.config;
+
+
+
+//import com.centurylink.pctl.mod.core.security.utils.JwtTokenUtil;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = "test";
+     //   String userName = JwtTokenUtil.getCurrentUserLogin();
+        return (userName != null ? userName : "");
+    }
+}
