66da068bfb96e6dc0d507a2674f17c27dc79b863
Begin Samuel <begin.samuel@hcl.com>
2016-10-27 21:12:24 +0530

    enabled logging in users

--- pctl.mod.user/src/main/java/com/centurylink/pctl/mod/user/config/WebSecurityConfig.java
+++ pctl.mod.user/src/main/java/com/centurylink/pctl/mod/user/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ import java.util.List;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements ResourceServerConfigurer {
     public static final String JWT_TOKEN_HEADER_PARAM = "X-Authorization";
-    public static final String JWT_TOKEN_USER_ENTRY_POINT = "/**" ;    // "/auth/**";
+    public static final String JWT_TOKEN_USER_ENTRY_POINT = "/auth/**" ;    // "/auth/**";
     public static final String JWT_TOKEN_USER_GET_TOKEN = "/auth/token/generate";
     public static final String LOGIN_USER_ENTRY_POINT = "/auth/user/login";
     public static final String LOGGED_OUT_ENTRY_POINT ="/auth/user/logoutSuccess";
