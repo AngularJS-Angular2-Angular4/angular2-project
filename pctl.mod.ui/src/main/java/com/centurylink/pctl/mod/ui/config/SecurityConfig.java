ea41b75f6c1eb516ce0f3532a6b43a4e51430f99
tsukhu <tsukhu@hcl.com>
2016-10-27 16:15:38 +0530

    reverted change

--- pctl.mod.ui/src/main/java/com/centurylink/pctl/mod/ui/config/SecurityConfig.java
+++ pctl.mod.ui/src/main/java/com/centurylink/pctl/mod/ui/config/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @Configuration
 @EnableResourceServer
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)
 public class SecurityConfig implements ResourceServerConfigurer {
 
     @Override
