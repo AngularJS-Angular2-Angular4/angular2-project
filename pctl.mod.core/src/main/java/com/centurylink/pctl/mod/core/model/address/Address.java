721e65714622549c3b5f5a80dad40cbb461724ad
Hari <haribabu.ka@hcl.com>
2016-10-31 20:36:31 +0530

    Address  Model moved to core package

---
+++ pctl.mod.core/src/main/java/com/centurylink/pctl/mod/core/model/address/Address.java
@@ -0,0 +1,49 @@
+package com.centurylink.pctl.mod.core.model.address;
+
+/**
+ * Created by haribabu.ka on 13-10-2016.
+ */
+public class Address {
+
+    private String locationName;
+    private String address;
+    private String street;
+    private String country;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    public Address() {
+
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+
+}
