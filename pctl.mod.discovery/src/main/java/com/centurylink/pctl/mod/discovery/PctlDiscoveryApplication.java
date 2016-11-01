d816b24d596109eeb4e91f44c6da4749bfa9316b
Begin Samuel <begin.samuel@hcl.com>
2016-11-01 11:59:42 +0530

    Excluded Mongo dependencies in discovery

--- pctl.mod.discovery/src/main/java/com/centurylink/pctl/mod/discovery/PctlDiscoveryApplication.java
+++ pctl.mod.discovery/src/main/java/com/centurylink/pctl/mod/discovery/PctlDiscoveryApplication.java
@@ -1,13 +1,17 @@
 package com.centurylink.pctl.mod.discovery;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by Tomasz Kucharzyk
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableEurekaServer
 public class PctlDiscoveryApplication {
 
